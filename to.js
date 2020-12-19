312d2e2030e44b357518aaacf37eb52a111648e5
Alexander <thesolve@mail.ru>
2020-12-19 12:20:21 +0300

    Немного исправил код

--- to.js
+++ to.js
@@ -19,13 +19,13 @@ function go()
 	console.log(filePath, truncateKey);
 	const data = fs.readFileSync(filePath);
 	const key = generateKey ? crypto.randomBytes(data.byteLength) : fs.readFileSync('key');
-	const dataInt8 = new Int8Array(data);
+	const dataUint8 = new Uint8Array(data);
 	const keyUint8 = new Uint8Array(key);
 	const result = new ArrayBuffer(data.byteLength);
-	const resultInt8 = new Int8Array(result);
-	for (let i = 0; i < resultInt8.length; i++)
+	const resultUint8 = new Uint8Array(result);
+	for (let i = 0; i < resultUint8.length; i++)
 	{
-		resultInt8[i] = keyUint8[i] ^ dataInt8[i];
+		resultUint8[i] = keyUint8[i] ^ dataUint8[i];
 	}
 	fs.writeFileSync('data', Buffer.from(result));
 	if (generateKey)
